f95466a6bd4e8a0ce38719223a8db8ebb2621627
Shawn Zimmermann <zimmermannshawn@gmail.com>
2021-12-03 00:18:05 -0500

    added code from tutorial

--- app/app.py
+++ app/app.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from flask_socketio import SocketIO
+from flask import render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder='./templates')
+#app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def sessions():  # put application's code here
+    return render_template('session.html') 
 
+def messageReceived(methods=['GET', 'POST']):
+    print('message was recieved!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('recieved my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", "8002", "debug")
+    # Listens to localhost:5000 by default
+    socketio.run(app, debug=True)
