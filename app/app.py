5206be36284f2200009c106635cec3c33ef171e1
Shawn Zimmermann <zimmermannshawn@gmail.com>
2021-12-03 01:46:55 -0500

    added redirect to dms page

--- app/app.py
+++ app/app.py
@@ -1,14 +1,30 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from flask import render_template
+from pymongo import MongoClient
+
 
 app = Flask(__name__, template_folder='./templates')
 #app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
-@app.route('/')
+client = MongoClient('mongodb://localhost:27017')
+db = client['C3WAT']
+accounts = db['accounts']
+
+@app.route('/', methods=['GET', 'POST'])
 def sessions():  # put application's code here
-    return render_template('session.html') 
+    try:
+        # Just have to put users here
+        users = ['paul', 'chris', 'david', 'fuck']
+        return render_template('session.html', data=users) 
+    except Exception as e:
+        return(str(e))
+
+@app.route('/dm', methods=['GET', 'POST'])
+def dms():
+    print("here in the dms")
+    return render_template('dm.html')
 
 def messageReceived(methods=['GET', 'POST']):
     print('message was recieved!!!')
