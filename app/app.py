caae405833fed3102cdf5a9ba5ebb578e9517375
kxvxnc <potatokevin123@gmail.com>
2021-12-06 12:07:37 -0500

    Add image upload to settings page

--- app/app.py
+++ app/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_socketio import SocketIO, send, emit
+from werkzeug.utils import secure_filename
 import json
 import bcrypt
+import os
 from pymongo import MongoClient
 
-
 # app.config['SECRET_KEY'] = 'secret!'
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__, template_folder='./templates')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 #app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
@@ -18,6 +22,7 @@ database = {
 client = MongoClient('mongodb://localhost:27017/')
 db = client['C3WAT']
 accounts = db['accounts']
+
 
 # loads homepage
 @app.route('/')
@@ -93,7 +98,28 @@ def register():
         return render_template('register.html', msg=msg)
     elif request.method == 'GET':
         return render_template('register.html')
-      
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/settings', methods=['GET', 'POST'])
+def settings():
+    if request.method == 'POST':
+        print(request.files)
+        if 'file' not in request.files:
+            return render_template('settings.html', msg="Image upload failed.")
+        file = request.files['file']
+        print(file)
+        if file.filename == '':
+            return render_template('settings.html', msg="No image uploaded.")
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('settings.html', msg="Image upload successful!")
+    elif request.method == 'GET':
+        return render_template('settings.html')
+
  # receive websocket from event
 @socketio.on('connection')
 def handle_my_custom_event(json_data):
