8148e4c23cc897d5e309eb55bae05915cc58278b
kxvxnc <potatokevin123@gmail.com>
2021-12-07 05:07:14 -0500

    Fix login/frontend and add session checking

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, render_template, request, redirect, session, url_for, flash
 from flask_socketio import SocketIO, send, emit
 from werkzeug.utils import secure_filename
 import json
@@ -6,81 +6,60 @@ import bcrypt
 import os
 from pymongo import MongoClient
 
-UPLOAD_FOLDER = 'app/static/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__, template_folder='./templates')
 app.config['SECRET_KEY'] = '59dadf181b39480eae2b277c981bfbda'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = 'app/static/uploads'
 socketio = SocketIO(app)
-
-database = {
-    "users": []
-}
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['C3WAT']
 accounts = db['accounts']
 
-# loads homepage
+database = {
+    "users": []
+}
+# Homepage; DM's work in progress
 @app.route('/')
-def home():  # put application's code here
-    users = ["andy", "ryan", "shawn", "kevin"]
-    return render_template('home.html', users=users)
-
-# @app.route('/')
-# def home():
-#    return render_template('index.html')
-
+def home():
+    if 'username' in session:
+        users = ["andy", "ryan", "shawn", "kevin"]
+        user = accounts.find_one({'username': session['username']})
+        return render_template('home.html', users=users, img=user['picturePath'])
+    else:
+        return render_template('index.html')
+# Login Implementation
 @app.route('/login',  methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
-        print('TESTING!')
-
+        # Compares submitted password to hash in DB
         if accounts.find_one({'username' : username}):
             login_user = accounts.find_one({'username': username})
             plain_password = request.form['password'].encode('utf8')
-            hashed_password = bcrypt.hashpw(plain_password, bcrypt.gensalt())
+            hashed_password = login_user['password']
             if bcrypt.checkpw(plain_password, hashed_password):
-                msg = 'Succesfully logged in!'
                 session['username'] = username
-                print("Setting session username to: " + username)
-                return render_template('home.html')
+                print("Setting session username: " + username)
+                flash('Successfully logged in!')
+                return redirect(url_for('home'))
+            else:
+                flash('Wrong password.')
+                return redirect(url_for('login'))
         else:
-            msg = 'Username was not found!'
-            return render_template('login.html', msg=msg)
-
+            flash('Wrong username.')
+            return redirect(url_for('login'))
     elif request.method == 'GET':
         return render_template('login.html')
-
-
-
-#@app.route('/', methods=['GET', 'POST'])
-#def sessions():  # put application's code here
-#    try:
-#        # Just have to put users here
-#        users = ['paul', 'chris', 'david', 'fuck']
-#        return render_template('session.html', data=users) 
-#    except Exception as e:
-#        return(str(e))
-
-@app.route('/dm', methods=['GET', 'POST'])
-def dms():
-    print("here in the dms")
-    return render_template('dm.html')
-
-def messageReceived(methods=['GET', 'POST']):
-    print('message was recieved!!!')
-
-
+# Register implementation
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
-
         if accounts.find_one({'username': username}):
-            msg = 'Username already exists.'
+            flash('Username already exists.')
+            return redirect(url_for('register'))
         else:
             plain_password = request.form['password'].encode('utf8')
             hashed_password = bcrypt.hashpw(plain_password, bcrypt.gensalt())
@@ -90,44 +69,57 @@ def register():
                 'picturePath': 'uploads/default.png'
             }
             if accounts.insert_one(account).inserted_id:
-                msg = 'Account has been created!'
+                flash('Account has been created!')
+                return redirect(url_for('login'))
             else:
-                msg = 'There was an error creating your account.'
-        
-        return render_template('register.html', msg=msg)
+                flash('There was an error creating your account.')
+                return redirect(url_for('register'))
     elif request.method == 'GET':
         return render_template('register.html')
+# Logout implementation
+@app.route('/logout', methods=['GET'])
+def logout():
+    print("Clearing session.")
+    session.clear()
+    return redirect(url_for('home'))
 
+# Helper function for image upload
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# File upload code source: https://flask.palletsprojects.com/en/2.0.x/patterns/fileuploads/
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():
-    if request.method == 'POST':
-        print(request.files)
-        if 'file' not in request.files:
-            return render_template('settings.html', msg="Image upload failed.")
-        file = request.files['file']
-        print(file)
-        if file.filename == '':
-            return render_template('settings.html', msg="No image uploaded.")
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            accounts.update_one({'username': session['username']}, {'$set': {'picturePath': "uploads/"+filename}})
-            return redirect(url_for('settings'))
-    elif request.method == 'GET':
-        user = accounts.find_one({'username': session['username']})
-        return render_template('settings.html', img=user['picturePath'])
+    if 'username' in session:
+        if request.method == 'POST':
+            # Error checking for file upload
+            if 'file' not in request.files:
+                flash('Image upload failed.')
+                return redirect(url_for('settings'))
+            file = request.files['file']
+            if file.filename == '':
+                flash("No image uploaded.")
+                return redirect(url_for('settings'))
+            # Save image to server directory and update the path in DB
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                accounts.update_one({'username': session['username']}, {'$set': {'picturePath': "uploads/"+filename}})
+                return redirect(url_for('settings'))
+        elif request.method == 'GET':
+            user = accounts.find_one({'username': session['username']})
+            return render_template('settings.html', img=user['picturePath'])
+    else:
+        flash('Please login to view this page.')
+        return redirect(url_for('home'))
 
- # receive websocket from event
+# receive websocket from event
 @socketio.on('connection')
 def handle_my_custom_event(json_data):
     print('received json: ' + json_data)
     print(request.sid)
     database["users"] += request.sid
-    
 
 # receive direct message and send to user
 @socketio.on('send_msg')
@@ -144,7 +136,14 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('recieved my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
 
-    
+@app.route('/dm', methods=['GET', 'POST'])
+def dms():
+    print("here in the dms")
+    return render_template('dm.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was recieved!!!')
+
 if __name__ == '__main__':
     socketio.run(app, "0.0.0.0", "8002", debug=True)
 
