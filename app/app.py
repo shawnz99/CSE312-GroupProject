470548a5a47735ac2d18b35bc9f6eb538a994b98
kxvxnc <potatokevin123@gmail.com>
2021-12-10 17:34:36 -0500

    Image upload bug fix

--- app/app.py
+++ app/app.py
@@ -3,23 +3,20 @@
 # [X] Next to each user in the list is an option to send them a DM. When a DM is sent to a user, 
 # [X] a JavaScript alert appears containing the message and the username of the sender. 
 # [X] Since that user has an option to send a DM to this user from their list,the option to reply exists
-# [ ] On the homepage of the app, provide an area where users can post text messages. 
-# [ ] Users can click a button to upvote each post via WebSockets which can be seen by all users without a refresh
+# [X] On the homepage of the app, provide an area where users can post text messages. 
+# [X] Users can click a button to upvote each post via WebSockets which can be seen by all users without a refresh
 # [X] Users can create an account and upload a profile picture (which they can change). 
 # [X] In the list of currently logged in users, display each users’ profile picture
 #
 from flask import Flask, render_template, request, redirect, session, url_for, flash
-from flask_socketio import SocketIO, send, emit
-from werkzeug.utils import secure_filename
+from flask_socketio import SocketIO, emit
+import os
 import json
 import bcrypt
-import os
 import secrets
 from pymongo import MongoClient
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-
 
 app = Flask(__name__, template_folder='./templates')
 # Set-Cookie options
@@ -29,6 +26,7 @@ app.config.update(
 )
 app.config['SECRET_KEY'] = '59dadf181b39480eae2b277c981bfbda'
 app.config['UPLOAD_FOLDER'] = '/app/static/uploads'
+
 socketio = SocketIO(app, logger=True)
 
 client = MongoClient('mongo')
@@ -37,9 +35,7 @@ accounts = db['accounts']
 votes = {}
 id = 0 ## This is for the chat so there is a id for each message to grab for the upvotes
 
-
 # Adding security headers to all responses
-
 @app.after_request
 def apply_headers(response):
     response.headers["X-Frame-Options"] = "SAMEORIGIN"
@@ -99,18 +95,6 @@ def login():
         session['xsrf_token'] = xsrf_token
         return render_template('login.html', xsrf_token=xsrf_token)
 
-
-
-#@app.route('/', methods=['GET', 'POST'])
-#def sessions():  # put application's code here
-#    try:
-#        # Just have to put users here
-#        users = ['paul', 'chris', 'david', 'fuck']
-#        return render_template('session.html', data=users)
-#    except Exception as e:
-#        return(str(e))
-
-
 # Register implementation
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -152,13 +136,10 @@ def logout():
     flash('Successfully logged out.')
     return redirect(url_for('home'))
 
-
-# Helper function for image upload
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 # File upload code source: https://flask.palletsprojects.com/en/2.0.x/patterns/fileuploads/
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():
     if 'username' in session:
@@ -173,8 +154,8 @@ def settings():
                 return redirect(url_for('settings'))
             # Save image to server directory and update the path in DB
             if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                print(os.getcwd())
+                # Randomize filename to prevent overrides of same image names
+                filename = secrets.token_urlsafe(16) + '.' + file.filename.rsplit('.', 1)[1].lower()
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 accounts.update_one({'username': session['username']}, {'$set': {'picturePath': "uploads/"+filename}})
                 return redirect(url_for('settings'))
@@ -200,7 +181,6 @@ def disconnect():
     if 'username' in session:
         accounts.update_one({'username': session['username']}, {'$set': {'loggedIn': 'false'}})
 
-
 # Receive direct message and send to user
 @socketio.on('send_msg')
 def send_msg(json_data):
@@ -215,25 +195,21 @@ def send_msg(json_data):
         send_data = json.dumps({'sender': "Server", 'msg': data['username'] + " is offline or in settings, please refresh page."})
         emit("receive_msg", send_data, to=request.sid)
 
-
 @socketio.on('my event')
 def handle_message(data):
     global votes
     global id 
-
     # Setting a unique ID for each message to be accessed by the vote
     id = id + 1
     votes[id] = 0 
     data['votes'] = votes[id]
     data['id'] = id 
     
-
     emit('my response', data , broadcast=True)
 
 @socketio.on('vote')
 def handle_event(div_id):
     global votes  
-
     # votes[id] isnt set here yet?
     votes[div_id] = votes[div_id] + 1
 
